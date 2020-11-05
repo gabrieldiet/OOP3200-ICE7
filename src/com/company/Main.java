7a0b4c2f951fcf2035834d2c0632d9aa3766d5cf
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-11-04 20:40:28 -0500

    Final version II

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,11 @@
  * @date Nov 04, 2020
  * @description Introduction to Java code
  */
+
 package com.company;
 
-public class Main {
-    public Main() {
-    }
-
+public class Main
+{
     public static void main(String[] args) {
         Person person = new Person("Tom", 40);
         person.saysHello();
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,54 +1,82 @@
+/**
+ * @author Gabriel Dietrich, 100764733
+ * @date Nov 04, 2020
+ * @description Introduction to Java code
+ */
 
 package com.company;
 
-public class Person {
+public class Person
+{
+    // Private instance variables
     private String m_name;
     private int m_age;
 
-    public String getName() {
+    // Public properties
+    public String getName()
+    {
         return this.m_name;
     }
 
-    public void setName(String m_name) {
+    public void setName(String m_name)
+    {
         this.m_name = m_name;
     }
 
-    public int getAge() {
+    public int getAge()
+    {
         return this.m_age;
     }
 
-    public void setAge(int m_age) {
+    public void setAge(int m_age)
+    {
         this.m_age = m_age;
     }
 
-    public Person() {
+    // Constructors
+
+    // Default constructor sets the same to "unknown" and age to 0
+    public Person()
+    {
         this.m_name = "unknown";
         this.m_age = 0;
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age)
+    {
         this.m_name = name;
         this.m_age = age;
     }
 
-    public Person(String name) {
+    public Person(String name)
+    {
         this.m_name = name;
         this.m_age = 0;
     }
 
-    public Person(int age) {
+    public Person(int age)
+    {
         this.m_name = "unknown";
         this.m_age = age;
     }
 
-    public void saysHello() {
+    // Methods
+    public void saysHello()
+    {
         System.out.println(this.m_name + " says Hello!");
     }
 
-    public String toString() {
+    /**
+     * This method overrides the built-in ToString() method and returns the Person properties
+     * @return {String}
+     */
+    @Override
+    public String toString()
+    {
         String outputString = "";
         outputString = outputString + "Name: " + this.m_name + " ";
         outputString = outputString + "Age: " + this.m_age;
+
         return outputString;
     }
 }
